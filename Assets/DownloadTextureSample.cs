51c19fa8be0da3d6aa64eaa85e0d876199a1e3eb
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2017-03-26 15:59:17 +0900

    first commit

---
+++ Assets/DownloadTextureSample.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DownloadTextureSample : MonoBehaviour {
+
+    public GameObject PhotoView;
+    public string Uri;
+
+    bool initialized = false;
+
+	IEnumerator Start () {
+        yield return StartCoroutine(download());
+        initialized = true;
+	}
+
+    IEnumerator download() {
+        var request = UnityWebRequest.GetTexture(Uri);
+        yield return request.Send();
+        if (request.isError) {
+            Debug.Log(request.error);
+        } else {
+            var dlHandler = (DownloadHandlerTexture)request.downloadHandler;
+            var tex = dlHandler.texture;
+            Debug.Log(string.Format("{0}, {1}", tex.width, tex.height));
+            var obj = Instantiate(PhotoView);
+            obj.GetComponent<Renderer>().material.mainTexture = tex;
+            Debug.Log("PhotoView initialized");
+        }
+    }
+	
+	void Update () {
+        if (!initialized) {
+            return;
+        }
+        Debug.Log("Update");
+	}
+}
